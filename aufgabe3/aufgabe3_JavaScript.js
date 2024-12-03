2bc3dab7694ea99351fcf08df2d54eec7766817a
mimug <mimug77@gmail.com>
2024-12-03 15:13:06 +0100

    aufgabe3> paralaxe, foregroundbilder

--- aufgabe3/aufgabe3_JavaScript.js
+++ aufgabe3/aufgabe3_JavaScript.js
@@ -1,1 +1,22 @@
+window.addEventListener('scroll', function () {
+  var scrollPosition = window.scrollY
+  var background = document.querySelector('.body')
+  var foreground = document.querySelector('.foreground')
+  background.style.backgroundPositionY = -scrollPosition * 0.5 + 'px'
+})
 
+// window.addEventListener('scroll', function() {
+//   var scrollPosition = window.scrollY;
+//   var background = document.querySelector('.body')
+//   var foreground = document.querySelector('.foreground');
+//   var main__wrapper = document.querySelector('.main__wrapper');
+
+//   // Bewegt den Hintergrund (body) langsamer
+//   document.body.style.backgroundPositionY = -(scrollPosition * 0.3) + 'px';
+
+//   // Bewegt den Hauptinhalt normal
+//   main__wrapper.style.transform = 'translateY(' + (-scrollPosition) + 'px)';
+
+//   // Bewegt den Foreground schneller
+//   foreground.style.transform = 'translateY(' + (-scrollPosition * 1.2) + 'px)';
+// });
